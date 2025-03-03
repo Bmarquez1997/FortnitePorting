df177cf4a97d956fdf123fd5efb8ff817c058c3f
Benjamin Marquez <bmarquez1997@gmail.com>
2025-03-03 09:45:33 -0500

    Updating version to match base FP

--- FortnitePorting/Globals.cs
+++ FortnitePorting/Globals.cs
@@ -9,7 +9,7 @@ namespace FortnitePorting;
 public static class Globals
 {
     public static string VersionString => Version.GetDisplayString(EVersionStringType.IdentifierPrefix);
-    public static readonly FPVersion Version = new(3, 1, 1, identifier: "Mutables");
+    public static readonly FPVersion Version = new(3, 0, 7, 1, identifier: "Mutables");
     public const string OnlineTag = "FortnitePorting";
     
     public static readonly FilePickerFileType MappingsFileType = new("Unreal Mappings") { Patterns = [ "*.usmap" ] };
