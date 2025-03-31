30d8db16ed4ddfcfbc2e076e081dd5f559cc7995
Benjamin Marquez <bmarquez1997@gmail.com>
2025-03-31 12:42:18 -0400

    Initial Properties view json folding logic

--- FortnitePorting/Windows/PropertiesPreviewWindow.axaml.cs
+++ FortnitePorting/Windows/PropertiesPreviewWindow.axaml.cs
@@ -1,15 +1,11 @@
-using System;
-using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.VisualTree;
-using CUE4Parse.UE4.Assets.Exports;
-using CUE4Parse.UE4.Assets.Exports.Texture;
-using FortnitePorting.Extensions;
+using Avalonia.Media;
+using AvaloniaEdit.Folding;
+using FortnitePorting.Application;
+using FortnitePorting.Models;
 using FortnitePorting.Services;
 using FortnitePorting.Shared.Extensions;
 using FortnitePorting.Shared.Framework;
-using FortnitePorting.Shared.Services;
-using FortnitePorting.ViewModels;
 using FortnitePorting.WindowModels;
 
 namespace FortnitePorting.Windows;
@@ -25,13 +21,33 @@ public partial class PropertiesPreviewWindow : WindowBase<PropertiesPreviewWindo
 
         WindowModel.AssetName = name;
         WindowModel.PropertiesJson = json;
-
     }
 
     protected override void OnLoaded(RoutedEventArgs e)
     {
         base.OnLoaded(e);
         Editor.TextArea.TextView.BackgroundRenderers.Add(new IndentGuideLinesRenderer(Editor));
+        
+        var foldingManager = FoldingManager.Install(Editor.TextArea);
+        var foldingStrategy = new JsonFoldingStrategy();
+        foldingStrategy.UpdateFoldings(foldingManager, Editor.Document);
+        
+        SetMarginColors(foldingManager);
+    }
+
+    //TODO: How does this get applied?
+    private void SetMarginColors(FoldingManager manager)
+    {
+        var brush = new SolidColorBrush(AppSettings.Current.Theme.BackgroundColor);
+        var hover = new SolidColorBrush(AppSettings.Current.Theme.AccentColor);
+        var margin = new FoldingMargin
+        {
+            FoldingManager = manager,
+            FoldingMarkerBrush = brush,
+            FoldingMarkerBackgroundBrush = brush,
+            SelectedFoldingMarkerBrush = hover,
+            SelectedFoldingMarkerBackgroundBrush = hover
+        };
     }
 
     public static void Preview(string name, string json)
